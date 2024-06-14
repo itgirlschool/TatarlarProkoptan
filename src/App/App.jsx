4157e58738846d59eac234eac10a5b2955832615
ksvaska <vas25ks@gmail.com>
2024-06-14 16:45:02 +0300

    homepage

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,10 @@
 import style from "./App.module.scss";
+import HomePage from "../Components/HomePage/HomePage";
 
 function App() {
-  return <div></div>;
+  return <div>
+    <HomePage/>
+  </div>;
 }
 
 export default App;
