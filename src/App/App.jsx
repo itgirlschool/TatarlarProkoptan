4c6bfdd23c06a4c926b245f72e9308c1f83c648b
SerovaKaterina <k.serova.k@yandex.ru>
2024-06-19 16:17:01 +0300

    add blocks

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,19 @@
-import style from "./App.module.scss";
-
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Culture from '../components/Culture/Culture';
 
 function App() {
-  return <div>
- 
-  </div>;
+  console.log('App component rendered');
+  return (
+    <Router>
+      <div>
+        <Culture />
+      <Routes>
+        <Route path="/culture" element={<Culture />} />
+      </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Culture/Culture.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Culture.module.scss';
+
+function Culture() {
+  return (
+    <div className={styles.culture}>
+      <h1>Культура</h1>
+      <div className={styles.blocksContainer}>
+        <div className={styles.block}>Культура татар Прокопьевска</div>
+        <div className={styles.block}>Хранители традиций</div>
+        <div className={styles.block}>Мечеть</div>
+        <div className={styles.block}>Наша национальная кухня</div>
+        <div className={styles.block}>Наши партнеры</div>
+      </div>
+    </div>
+  );
+}
+
+export default Culture;
