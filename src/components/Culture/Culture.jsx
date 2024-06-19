da1b60dbf55a758b4e282ede6b56b6198571be56
SerovaKaterina <k.serova.k@yandex.ru>
2024-06-19 21:02:11 +0300

    add styles block

--- src/components/Culture/Culture.jsx
+++ src/components/Culture/Culture.jsx
@@ -6,8 +6,8 @@ function Culture() {
     <div className={styles.culture}>
       <h1>Культура</h1>
       <div className={styles.blocks_container}>
-        <div className={styles.block}>Культура татар Прокопьевска</div>
-        <div className={styles.block}>Хранители традиций</div>
+        <div className={styles.block5}>Культура татар Прокопьевска</div>
+        <div className={styles.block2}>Хранители традиций</div>
         <div className={styles.block}>Мечеть</div>
         <div className={styles.block}>Наша национальная кухня</div>
         <div className={styles.block}>Наши партнеры</div>
