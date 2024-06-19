8d6922d75eba07c99d5389137288b9215feb6f3d
Bema <138109349+Bemadler@users.noreply.github.com>
2024-06-20 01:31:25 +0600

    new ornament design

--- src/Сomponents/AuthorizationPage/AuthorizationPage.jsx
+++ src/Сomponents/AuthorizationPage/AuthorizationPage.jsx
@@ -5,57 +5,81 @@ import * as yup from "yup";
 import style from "./AuthorizationPage.module.scss";
 
 const schema = yup.object().shape({
-  email: yup.string().email("Не верный email адрес").required("Требуется email"),
-  password: yup.string().min(6, "Пароль должен содердать от 6 символов").required("Требуется пароль"),
+  email: yup
+    .string()
+    .email("Не верный email адрес")
+    .required("Требуется email"),
+  password: yup
+    .string()
+    .min(6, "Пароль должен содердать от 6 символов")
+    .required("Требуется пароль"),
 });
 
 const AuthorizationPage = () => {
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
     resolver: yupResolver(schema),
   });
 
-
   const onSubmit = async (data) => {
     const { email, password } = data;
-
-
   };
   return (
     <div className={style.container}>
-              <img src="src/assets/logo/logo.png" 
+      {/* <img
+        src="src/assets/logo/logo.png"
         className={style.image__logo}
         alt="logo"
-        />
+      /> */}
+      <img
+        src="src/assets/pictures/tatar_ornament.png"
+        className={style.tatar__ornament}
+        alt="decoration"
+      />
       <h2 className={style.header}>Авторизация</h2>
       <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={style.input__wrapper}>
-          <label className={style.label} htmlFor="email">Email</label>
+          <label className={style.label} htmlFor="email">
+            Email
+          </label>
           <input
             className={errors.email ? style.input__empty : style.input}
             type="email"
             id="email"
             {...register("email")}
           />
-          {errors.email && <p className={style.error}>{errors.email.message}</p>}
+          {errors.email && (
+            <p className={style.error}>{errors.email.message}</p>
+          )}
         </div>
         <div className={style.input__wrapper}>
-          <label className={style.label} htmlFor="password">Пароль</label>
+          <label className={style.label} htmlFor="password">
+            Пароль
+          </label>
           <input
             className={errors.password ? style.input__empty : style.input}
             type="password"
             id="password"
             {...register("password")}
           />
-          {errors.password && <p className={style.error}>{errors.password.message}</p>}
+          {errors.password && (
+            <p className={style.error}>{errors.password.message}</p>
+          )}
         </div>
         <div className={style.button__container}>
-        <button type="submit" className={style.button__submit} disabled={isSubmitting}>
-          Подтвердить
-        </button>
+          <button
+            type="submit"
+            className={style.button__submit}
+            disabled={isSubmitting}
+          >
+            Подтвердить
+          </button>
         </div>
-
+       
       </form>
-
     </div>
   );
 };
