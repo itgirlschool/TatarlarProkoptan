1cb4af0f254c67a111f195505c2df7c4348cdea3
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2024-06-20 18:30:53 -0500

    Add Event Page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,11 @@
 import style from "./App.module.scss";
 import Footer from "../Components/Footer/Footer";
+import EventsPage from "../Pages/EventsPage/EventsPage";
 
 function App() {
   return (
     <div>
+      <EventsPage />
       <Footer />
     </div>
   );
---
+++ src/Pages/EventsPage/EventsPage.jsx
@@ -0,0 +1,13 @@
+import EventsCalendar from "./Calendar";
+// import style from '../EventsPage/EventsPage.module.scss';
+
+const EventsPage = () => {
+    return(
+        <div>
+            <h3>Узнай о наших ближайших мероприятиях</h3>
+            <EventsCalendar />
+        </div>
+    )
+}
+
+export default EventsPage;
