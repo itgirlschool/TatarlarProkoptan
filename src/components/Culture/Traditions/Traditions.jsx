e4649bae0f2dff3efecb0a5306c3658d8f8b4299
SerovaKaterina <k.serova.k@yandex.ru>
2024-06-22 11:41:08 +0300

    add blocks cuisine,traditions

--- src/components/Culture/Traditions/Traditions.jsx
+++ src/components/Culture/Traditions/Traditions.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import imagesData from '../../../Services/CultureData/culture.json';
+import styles from './Traditions.module.scss'
 function Traditions() {
   return (
-    <div>
+    <div className={styles.traditions}>
       <h1>Хранители традиций</h1>
-      <p>Здесь будет информация.</p>
+      <div className={styles.traditions_images}>
+        {imagesData.map((image, index) => (
+          <div key={index} className={styles.traditions_image_wrapper}>
+            <img src={image.src} alt={image.alt} className={styles.traditions_image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
