82da856ff3b5e5aa86a2731c08580750dffdfed2
rkvrpl <rkvrpl@icloud.com>
2024-06-23 16:04:46 +0300

    deleted my component from app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,10 @@
 import style from "./App.module.scss";
 import Footer from "../Components/Footer/Footer";
-import WeAreTogether from '../Components/WeAreTogether/WeAreTogether'
 
 
 function App() {
   return (
     <div>
-      <WeAreTogether />
       <Footer />
     </div>
   );
