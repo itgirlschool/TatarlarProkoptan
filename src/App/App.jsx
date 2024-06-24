0f0838cb13c7257171fdd80f0d37e66d4e64c76f
Bema <138109349+Bemadler@users.noreply.github.com>
2024-06-24 21:33:30 +0600

    clear app.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import AuthorizationPage from "../Сomponents/AuthorizationPage/AuthorizationPage";
 import style from "./App.module.scss";
-import Footer from "../Components/Footer/Footer";
 
 function App() {
   return (
