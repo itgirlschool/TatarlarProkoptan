0f55334d361eed403282caca8f53a409947b78fc
ksvaska <vas25ks@gmail.com>
2024-06-24 16:47:44 +0300

    fixed app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,10 @@
 import style from "./App.module.scss";
-import HomePage from '../Components/HomePage/HomePage';
-import Footer from "../Components/Footer/Footer";
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
-      <Footer />
+  
     </div>
   );
 }
