debbe511e1b5479a786b931cc6a7b30dc036c218
LeonovEgorF <egor1908608@gmail.com>
2024-06-27 17:29:28 +0300

    add services

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initializeApp } from "firebase/app";
+import firebase from  'firebase/compat/app';
+import 'firebase/compat/database';
 import firebaseConfig from  "../firebaseConfig.js";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App/App";
 import './style/index.scss';
 
-
 initializeApp(firebaseConfig);
+export const dbFB = firebase.initializeApp(firebaseConfig).database()
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
