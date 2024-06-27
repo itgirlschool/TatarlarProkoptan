744ac0be828dac170a3797eb58e3964a5bb50cde
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2024-06-27 13:54:51 -0500

    Add Events page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,7 +4,6 @@ import  {HomePage,AboutUs,Charity,Culture,Events,Partners,Sabantui,WeAreTogether
 import Header from "../Components/Header/Header.jsx";
 import Footer from "../Components/Footer/Footer.jsx";
 
-
 import style from "./App.module.scss";
 
 
