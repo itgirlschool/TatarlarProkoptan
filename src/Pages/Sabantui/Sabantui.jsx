ef963537bd468192dde4ecb118cde7955cecb9ce
Irina <il064330@gmail.com>
2024-06-29 10:46:31 +0300

    feat: update styles

--- src/Pages/Sabantui/Sabantui.jsx
+++ src/Pages/Sabantui/Sabantui.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import style from "./Sabantui.module.scss";
+//import style from "./Sabantui.module.scss";
 import SabantuyPage from "./SabantuyPage/SabantuyPage.jsx";
 import SabantuyCollection from "../Sabantui/SabantuyCollection/SabantuyCollection.jsx";
 import SabantuyCurrentYear from "../Sabantui/SabantuyCurrentYear/SabantuyCurrentYear.jsx";
 
 const Sabantui = () => {
-  <div>
-    <Routes>
-      <Route path="/" element={<SabantuyPage />} />
-      <Route path="/collection" element={<SabantuyCollection />} />
-      <Route path="/current" element={<SabantuyCurrentYear />} />
-    </Routes>
-  </div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<SabantuyPage />} />
+        <Route path="/collection" element={<SabantuyCollection />} />
+        <Route path="/current" element={<SabantuyCurrentYear />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Sabantui;
