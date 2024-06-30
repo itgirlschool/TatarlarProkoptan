c0089d160858f12fb93499589c3272d07eafcb90
SerovaKaterina <k.serova.k@yandex.ru>
2024-06-30 14:34:15 +0300

    returned original App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
-import TatarCulture from '../Pages/Culture/TatarCulture/TatarCulture';
-import Mosques from '../Pages/Culture/Mosques/Mosques';
-import Traditions from '../Pages/Culture/Traditions/Traditions';
-import Cuisine from '../Pages/Culture/Cuisine/Cuisine';
-import Partners from '../Pages/Culture/Partners/Partners';
 import {
   HomePage,
   Charity,
   Culture,
   Events,
+  Partners,
   Sabantui,
   WeAreTogether,
 } from "../Pages";
@@ -27,11 +23,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/charity" element={<Charity />} />
           <Route path="/culture" element={<Culture />} />
-          <Route path="/tatar_culture" element={<TatarCulture />} />
-    <Route path="/mosques" element={<Mosques />} />
-    <Route path="/traditions" element={<Traditions />} />
-    <Route path="/cuisine" element={<Cuisine />} />
-    <Route path="/partners" element={<Partners />} />
           <Route path="/events" element={<Events />} />
           <Route path="/partners" element={<Partners />} />
           <Route path="/sabantui" element={<Sabantui />} />
