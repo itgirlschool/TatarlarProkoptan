5a652258ceba8606797a6ba6444b177d4709bddc
Irina <il064330@gmail.com>
2024-06-30 23:44:57 +0300

    refactor: change code in all files

--- src/Pages/Sabantui/Sabantui.jsx
+++ src/Pages/Sabantui/Sabantui.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-//import style from "./Sabantui.module.scss";
-import SabantuyPage from "./SabantuyPage/SabantuyPage.jsx";
-import SabantuyCollection from "../Sabantui/SabantuyCollection/SabantuyCollection.jsx";
-import SabantuyCurrentYear from "../Sabantui/SabantuyCurrentYear/SabantuyCurrentYear.jsx";
+import style from "./Sabantui.module.scss";
+import sabantuy from "../../../public/imgs/Sabantuy/main.jpg";
+import SabantuyBlocks from "../Sabantui/SabantuyBlocks/SabantuyBlocks";
 
-const Sabantui = () => {
+export default function Sabantui() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<SabantuyPage />} />
-        <Route path="/collection" element={<SabantuyCollection />} />
-        <Route path="/current" element={<SabantuyCurrentYear />} />
-      </Routes>
+    <div className={style.sabantuy__container}>
+      <h1 className={style.sabantuy_title}>Сабантуй</h1>
+      <img src={sabantuy} alt="Sabantuy" className={style.sabantuy__photo} />
+      <h2 className={style.sabantuy__text}>
+        <span className={style.sabantuy__name}> Сабантуй </span> - любимый
+        праздник татарского народа. На гулянии проводятся разные игры, в которых
+        участвуют и взрослые, и дети.
+      </h2>
+      <div>
+        <SabantuyBlocks />
+      </div>
+      <h2 className={style.sabantuy__text}>
+        На Сабантуе гостей принято угощать национальной выпечкой, а если
+        праздник отмечают в пригороде, то и жареным на открытом огне мясом или
+        пловом из казана. Одновременно проводятся состязания певцов, чтецов,
+        танцоров. Характерной чертой всех состязаний является то, что любой
+        присутствующий в любой момент может включиться в них.
+        <br /> Единство выступающих и зрителей делает Сабантуй настоящим
+        праздником.
+      </h2>
     </div>
   );
-};
-
-export default Sabantui;
+}
