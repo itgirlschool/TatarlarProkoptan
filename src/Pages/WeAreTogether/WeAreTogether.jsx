bfe3ec5bb2297f5222252ffca1ef3f089d31cdf1
ksvaska <vas25ks@gmail.com>
2024-07-08 19:29:23 +0300

    modal+adaptiv

--- src/Pages/WeAreTogether/WeAreTogether.jsx
+++ src/Pages/WeAreTogether/WeAreTogether.jsx
@@ -2,10 +2,11 @@ import style from './WeAreTogether.module.scss'
 import childrenImg from '../../assets/images/children9.jpg';
 import helpImg from '../../assets/images/help.jpg';
 import galleryImg from '../../assets/images/gallery4.jpg';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ChildrenModule from './ChildrenModule'
 import HelpModal from './HelpModal';
 import GalleryModal from './GalleryModal';
+import { useNavigate } from "react-router-dom";
 
 
 const WeAreTogether = () => {
@@ -13,6 +14,46 @@ const WeAreTogether = () => {
 	const [activeChildren, setActiveChildren] = useState(false);
 	const [activeHelp, setActiveHelp] = useState(false);
 	const [activeGallery, setActiveGallery] = useState(false);
+	const [isMobile, setIsMobile] = useState(false);
+    const navigate = useNavigate();
+
+	useEffect(() => {
+		const checkWindowSize = () => {
+		  setIsMobile(window.innerWidth <= 720);
+		};
+	
+		checkWindowSize();
+		window.addEventListener("resize", checkWindowSize);
+	
+		return () => {
+		  window.removeEventListener("resize", checkWindowSize);
+		};
+	  }, []);
+
+	  const handleChildrenClick=()=>{
+		if(isMobile){
+			navigate('/childrenMobile');
+		} else{
+			setActiveChildren(true)
+		}
+
+	  }
+
+	  const handleHelpClick=()=>{
+		if(isMobile){
+			navigate('/helpMobile');
+		} else{
+			setActiveHelp(true)
+		}
+	  }
+
+	  const handleGalleryClick=()=>{
+		if(isMobile){
+			navigate('/galleryMobile');
+		} else{
+			setActiveGallery(true)
+		}
+	  }
 	
 	
 
@@ -26,21 +67,21 @@ const WeAreTogether = () => {
 			<article className={style.content__img}>
 				<div className={style.img__block}>
 				<img src={childrenImg} alt='наша молодежь' className={style.img} />
-				<h2 className={style.header__img} onClick={()=>setActiveChildren(true)}>Наша молодежь</h2> 
+				<h2 className={style.header__img} onClick={handleChildrenClick}>Наша молодежь</h2> 
 				 </div> 
 				 </article>
 				 <ChildrenModule active={activeChildren} setActive={setActiveChildren}/>
 			<article className={style.content__img}>
 			<div className={style.img__block}>
 				<img src={helpImg} alt='мы помогаем' className={style.img} />
-				<h2 className={style.header__img} onClick={()=>setActiveHelp(true)}>Помогаем в трудную минуту</h2>
+				<h2 className={style.header__img} onClick={handleHelpClick}>Помогаем в трудную минуту</h2>
 				 </div> 
 				 </article> 
 				<HelpModal active={activeHelp} setActive={setActiveHelp}/>
 			<article className={style.content__img}>
 			<div className={style.img__block}>
 				<img src={galleryImg} alt='наша галерея' className={style.img} />
-				<h2 className={style.header__img} onClick={()=>setActiveGallery(true)}>Галерея</h2> 
+				<h2 className={style.header__img} onClick={handleGalleryClick}>Галерея</h2> 
 				 </div> 
 				 </article>
 				 <GalleryModal active={activeGallery} setActive={setActiveGallery}/>
