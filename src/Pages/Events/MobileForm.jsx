7939bbb3ea8557e2d33b9f9fad6968a3e3b8d33d
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2024-07-08 13:25:42 -0500

    Fix styles

--- src/Pages/Events/MobileForm.jsx
+++ src/Pages/Events/MobileForm.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import style from './Events.module.scss';
 
 const MobileForm = (options) => {
-
+    const [value, setValue] = useState("Записаться");
+    const handleClick = (e) => {
+        setValue("Заявка отправлена!", value);
+        e.preventDefault();
+    }
     return  (
         <form className={style.mobileForm}>
             <div>{options.title}</div>
             <p className={style.mobile_events}>{options.label}</p>
-            <button className={style.button_events}>Записаться</button>
+            <button onClick={handleClick} className={style.button_events}>{value}</button>
         </form>
     )
 }
