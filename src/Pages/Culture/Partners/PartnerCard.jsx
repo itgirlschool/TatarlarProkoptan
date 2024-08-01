47f68270e1f2c7b43da1b5671ed52439cbe9f30e
SerovaKaterina <k.serova.k@yandex.ru>
2024-08-01 23:15:43 +0300

    add changes culture

--- src/Pages/Culture/Partners/PartnerCard.jsx
+++ src/Pages/Culture/Partners/PartnerCard.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './PartnerCard.module.scss';
-import { YMaps, Map,Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
+function PartnerCard({ title, description, link, phone, location, coordinates }) {
+  const [showMap, setShowMap] = useState(false);
 
-function PartnerCard  ({ title, description, link,phone,location, coordinates }) {
+  const handleShowMap = () => {
+    setShowMap(true);
+  };
+
   return (
     <div className={styles.card}>
       <div className={styles.content}>
         <h1 className={styles.title_h1}>{title}</h1>
         <p>{description}</p>
-        {link ? (
+        {link && (
           <a href={link} target="_blank" rel="noopener noreferrer">Подробнее</a>
-        ) : (
+        )}
+        {!link && phone && (
           <a href={`tel:${phone}`} target="_blank" rel="noopener noreferrer">Подробнее</a>
         )}
         {location && <p>{location}</p>}
         {coordinates && (
-          <YMaps>
-            <div className={styles.map}>
-              <Map defaultState={{ center: coordinates, zoom: 15 }} width="100%" height="100%" >
-                <Placemark geometry={coordinates} />
-              </Map>
-            </div>
-          </YMaps>
+          <div className={styles.map_container}>
+            {!showMap ? (
+              <button onClick={handleShowMap} className={styles.map_button}>
+                Посмотреть на карте
+              </button>
+            ) : (
+              <YMaps>
+                <div className={styles.map}>
+                  <Map defaultState={{ center: coordinates, zoom: 15 }} width="100%" height="100%">
+                    <Placemark geometry={coordinates} />
+                  </Map>
+                </div>
+              </YMaps>
+            )}
+          </div>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default PartnerCard;
