07fddd60248e03a3092453a026b02348bfcba496
ksvaska <vas25ks@gmail.com>
2024-08-06 14:07:00 +0300

    news page

--- src/Pages/News/NewsBanner.jsx
+++ src/Pages/News/NewsBanner.jsx
@@ -8,9 +8,13 @@ export default function NewsBanner(){
             return(
                 <div className={style.banner} key={item.id}>
                  <div className={style.banner_img}>
-                 <img className={style.img} src={item.img} alt="news" />
+                <a href="#"> 
+                <img className={style.img} src={item.img} alt="news"/>
+                </a>
              </div>
-             <p className={style.banner_text}>{item.text}</p>
+             <p className={style.banner_text}>{item.text}<br/>
+             <a href="#" className={style.news_link}>Читать полностью</a>
+             </p>
              </div>
             )
         })
