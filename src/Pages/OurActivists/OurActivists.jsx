6054591fe7ed76891b99720fb11ca03c575f1639
WeronikaGRIG <veronikaudot@gmail.com>
2024-08-08 14:22:46 +0200

    add frames for images; icjns frames; changes styles

--- src/Pages/OurActivists/OurActivists.jsx
+++ src/Pages/OurActivists/OurActivists.jsx
@@ -9,6 +9,8 @@ import nail from '../../assets/images/ourActivists/nail-ismagilovicz.png';
 import marat from '../../assets/images/ourActivists/marat-minnegarifovicz.png';
 import kongress from '../../assets/icon/kongress.svg';
 import vvk from '../../assets/icon/vvk.svg';
+import frameRight from '../../assets/icon/frameRight.svg';
+import frameLeft from '../../assets/icon/frameLeft.svg';
 
 const cards = [
     { id: 'a1', image: lilia, name: 'Лилия Идрисовна Якутович', jobTitle: 'Председатель правления Автономии' },
@@ -29,7 +31,11 @@ const OurActivists = () => {
                     {cards.map((card, id) => (
                         <div key={id} className={style.card}>
                             <div className={style.card__details}>
-                                <img src={card.image} alt={card.name} className={style.image} />
+                                <div className={style.frame}>
+                                    <img src={frameLeft} alt="Frame with ornament" />
+                                    <img src={card.image} alt={card.name} className={style.image} />
+                                    <img src={frameRight} alt="Frame with ornament" />
+                                </div>
                                 <h3 className={style.name}>{card.name}</h3>
                                 <p className={style.job}>{card.jobTitle}</p>
                             </div>
