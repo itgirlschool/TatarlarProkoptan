27dc59962692c0720e0c8e38e5b7992c3c37f2a8
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2024-08-13 12:43:43 -0500

    fix styles

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -2,20 +2,17 @@ import {React, useState} from 'react';
 import { options } from './dataEvents';
 import EventsForm from './EventsForm.jsx';
 import ModalFormSingUp from "../../Components/ModalSingUp/ModalFormSingUp.jsx";
-import { useNavigate } from "react-router-dom";
 import style from './Events.module.scss';
 
   const Events = () => {
     const [modalActive, setModalActive] = useState(false);
-    // const navigate = useNavigate();
     const handleClick = (e) => {
           setModalActive(true);
           e.preventDefault();
-
     }
       return <div className={style.eventsPage}>
       <h1 className={style.h1_events}>Узнай о наших ближайших мероприятиях</h1>
-      <> {!modalActive?
+      <> {modalActive?
             <ModalFormSingUp  />:
         <div className={style.eventsPageMobile}>
             {options.map((option)=> (
