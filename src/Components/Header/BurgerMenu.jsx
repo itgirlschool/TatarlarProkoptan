0f3aa68d61641c03d34b5ea58d9872536a3bfda6
Archik-K <ar.kryatova@yandex.ru>
2024-08-27 19:58:30 +0300

    the usual display of pages in charity is thoroughly configured, instead of modal windows. In the registration section, the container with the text and the input field has been fixed, which no longer goes beyond the limits. The task was completed: to make the burger menu an active item on the page we are on

--- src/Components/Header/BurgerMenu.jsx
+++ src/Components/Header/BurgerMenu.jsx
@@ -3,49 +3,132 @@ import { NavLink } from "react-router-dom";
 import styles from "./BurgerMenu.module.scss";
 
 const links = [
-	{ label: "О нас", path: "/" },
-	{ label: "Мероприятия", path: "/events" },
-	{ label: "Культура", path: "/culture" },
-	{ label: "Сабантуй", path: "/sabantui" },
-	{ label: "Благотворительность", path: "/charity" },
-	{ label: "Мы вместе", path: "/we-are-together" },
-	{ label: "Наши активисты", path: "/our-activists" },
-	{ label: "Контакты", path: "/contacts" },
-	{ label: "Новвости", path: "/news" },
-	{ label: "Выйти из аккаунта", path: "/authorizationpage" },
+	{
+		label: "О нас",
+		path: "/",
+	},
+	{
+		label:
+			"Мероприятия",
+		path: "/events",
+	},
+	{
+		label: "Культура",
+		path: "/culture",
+	},
+	{
+		label: "Сабантуй",
+		path: "/sabantui",
+	},
+	{
+		label:
+			"Благотворительность",
+		path: "/charity",
+	},
+	{
+		label:
+			"Мы вместе",
+		path:
+			"/we-are-together",
+	},
+	{
+		label:
+			"Наши активисты",
+		path:
+			"/our-activists",
+	},
+	{
+		label: "Контакты",
+		path: "/contacts",
+	},
+	{
+		label: "Новвости",
+		path: "/news",
+	},
+	{
+		label:
+			"Выйти из аккаунта",
+		path:
+			"/authorizationpage",
+	},
 ];
 
 function BurgerMenu() {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [
+		isMenuOpen,
+		setIsMenuOpen,
+	] =
+		useState(false);
 
-	const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+	const toggleMenu =
+		() =>
+			setIsMenuOpen(
+				!isMenuOpen
+			);
 
-	const isOpen = isMenuOpen ? styles.open : "";
+	const isOpen =
+		isMenuOpen
+			? styles.open
+			: "";
 
 	return (
 		<>
 			<button
 				className={`${styles.burger} ${isOpen}`}
-				onClick={toggleMenu}
+				onClick={
+					toggleMenu
+				}
 			></button>
-			<div className={`${styles.background} ${isOpen}`}></div>
-			<div className={`${styles.menu} ${isOpen}`}>
+			<div
+				className={`${styles.background} ${isOpen}`}
+			></div>
+			<div
+				className={`${styles.menu} ${isOpen}`}
+			>
 				<nav>
-					{links.map((link, index) => (
-						<NavLink
-							key={link.label}
-							to={link.path}
-							className={({ isActive }) =>
-								`${isMenuOpen ? styles.appear : ""} ${
-									link.label === "Выйти из аккаунта" ? styles.yellowText : ""
-								} ${isActive ? styles.active : ""}`
-							}
-							style={{ animationDelay: `0.${index + 1}s` }}
-							onClick={toggleMenu}
-						>
-							{link.label}
-						</NavLink>
-					))}
+					{links.map(
+						(
+							link,
+							index
+						) => (
+							<NavLink
+								key={
+									link.label
+								}
+								to={
+									link.path
+								}
+								className={({
+									isActive,
+								}) =>
+									`${
+										isMenuOpen
+											? styles.appear
+											: ""
+									} ${
+										link.label ===
+										"Выйти из аккаунта"
+											? styles.yellowText
+											: ""
+									} ${
+										isActive
+											? styles.active
+											: ""
+									}`
+								}
+								style={{
+									animationDelay: `0.${
+										index + 1
+									}s`,
+								}}
+								onClick={
+									toggleMenu
+								}
+							>
+								{link.label}
+							</NavLink>
+						)
+					)}
 				</nav>
 			</div>
 		</>
