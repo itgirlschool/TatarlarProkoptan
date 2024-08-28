43bfc810fff6b102b9cd66c73f8ec7d5d2ad6999
Archik-K <ar.kryatova@yandex.ru>
2024-08-28 17:42:05 +0300

    The main task has been completed: to make the burgers menu an active item on the page we are on. In addition: in the registration section, the container with the text and the input field has been fixed, which no longer goes beyond the acceptable limits. Also in the charity section, I have carefully configured the usual display of pages, instead of modal windows.

--- src/Components/Header/BurgerMenu.jsx
+++ src/Components/Header/BurgerMenu.jsx
@@ -1,65 +1,73 @@
+
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./BurgerMenu.module.scss";
 
 const links = [
-  { label: "О нас", path: "/" },
-  { label: "Мероприятия", path: "/events" },
-  { label: "Культура", path: "/culture" },
-  { label: "Сабантуй", path: "/sabantui" },
-  { label: "Благотворительность", path: "/charity" },
-  { label: "Мы вместе", path: "/we-are-together" },
-  { label: "Наши активисты", path: "/our-activists" },
-  { label: "Контакты", path: "/contacts" },
-  { label: "Новости", path: "/news" },
-  // { label: "Авторизация", path: "/authorizationpage" },
-   ];
+{ label: "О нас", path: "/" },
+{ label: "Мероприятия", path: "/events" },
+{ label: "Культура", path: "/culture" },
+{ label: "Сабантуй", path: "/sabantui" },
+{ label: "Благотворительность", path: "/charity" },
+{ label: "Мы вместе", path: "/we-are-together" },
+{ label: "Наши активисты", path: "/our-activists" },
+{ label: "Контакты", path: "/contacts" },
+{ label: "Новости", path: "/news" },
+ // { label: "Авторизация", path: "/authorizationpage" },
+];
 
 function BurgerMenu() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false); 
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  const isOpen = isMenuOpen ? styles.open : "";
-  const handleLogin = () => {
-    setIsAuthenticated(true); 
-    toggleMenu();  };
+ const [isMenuOpen, setIsMenuOpen] = useState(false);
+ const [isAuthenticated, setIsAuthenticated] = useState(false);
+ const navigate = useNavigate();
 
-  const handleLogout = () => {
-    setIsAuthenticated(false); 
-    navigate("/");
-  };
-  
-  
-  const visibleLinks = isAuthenticated
-    ? [...links, { label: "Выйти", path: "/", onClick: handleLogout }]
-    : [...links, { label: "Авторизация", path: "/authorizationpage", onClick: handleLogin }];
+ const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+ const isOpen = isMenuOpen ? styles.open : "";
 
-  return (
-    <>
-      <button
-        className={`${styles.burger} ${isOpen}`}
-        onClick={toggleMenu}
-      ></button>
-      <div className={`${styles.background} ${isOpen}`}></div>
-      <div className={`${styles.menu} ${isOpen}`}>
-        <nav>
-          {visibleLinks.map((link, index) => (
-            <NavLink
-              key={link.label}
-              to={link.path}
-              className={`${isMenuOpen ? styles.appear : ""} ${
-                link.label === "Выйти" ? styles.yellowText : ""
-              }`}
-              style={{ animationDelay: `0.${index + 1}s` }}
-              onClick={link.onClick || toggleMenu}
-            >
-              {link.label}
-            </NavLink>
-          ))}
-        </nav>
-      </div>
-    </>
-  );
+ const handleLogin = () => {
+  setIsAuthenticated(true);
+  toggleMenu();
+  navigate("/"); 
+ };
+
+ const handleLogout = () => {
+  setIsAuthenticated(false);
+  toggleMenu();
+  navigate("/"); 
+ };
+
+ const visibleLinks = isAuthenticated
+  ? [...links, { label: "Выйти", path: "/", onClick: handleLogout }]
+  : [...links, { label: "Авторизация", path: "/authorizationpage", onClick: handleLogin }];
+
+ return (
+  <>
+   <button
+    className={`${styles.burger} ${isOpen}`}
+    onClick={toggleMenu}
+   ></button>
+   <div className={`${styles.background} ${isOpen}`}></div>
+   <div className={`${styles.menu} ${isOpen}`}>
+    <nav>
+     {visibleLinks.map((link, index) => (
+      <NavLink
+      key={link.label}
+       to={link.path}
+       className={({ isActive }) =>
+        `${isMenuOpen ? styles.appear : ""} ${
+         link.label === "Выйти" ? styles.yellowText : ""
+       } ${isActive ? styles.active : ""}`
+       }
+       style={{ animationDelay: `0.${index + 1}s` }}
+       onClick={link.onClick || toggleMenu}
+      >
+       {link.label}
+      </NavLink>
+     ))}
+    </nav>
+   </div>
+  </>
+ );
 }
 
 export default BurgerMenu;
