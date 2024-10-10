b6ae61759caccc7c0319d86ba7e5b9be96dabcae
Aizhan-px <aizhan.mysakanova.ch@gmail.com>
2024-10-10 16:59:10 +0200

    add  new admin page

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -1,19 +1,62 @@
 import style from "./AdminPage.module.scss";
-import {useNavigate} from "react-router-dom";
+import { Routes, useNavigate } from "react-router-dom";
+import App from "../../App/App";
+import Charity from "../Charity/Charity";
+import News from "../News/News";
+import { Link } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
 
-const AdminPage = () => {
-const navigate = useNavigate(); 
+
+
+const AdminPages = () => {
+  
+
+  const navigate = useNavigate();
   const handleButtonClick = () => {
-    navigate("/"); 
+    navigate("/");
   };
-    return (
-        <div className={style.container}>
-             <button className={style.button} onClick={handleButtonClick}>
-              На главную станицу
-            </button>
-            <p className={style.title}> Здесь будет страница администратора</p>
-        </div>
-    )
-}
+  
+  return (
+    
+    <div className={style.container}>
+       <Header/>
+      
+   <div className="adminBlock_container">
+    
+      <div className={style.charity}>
+            <Link to="auth/charity">Благотворительность</Link>
+          </div>
 
-export default AdminPage;
+     <div className={style.autonomy}>
+            <Link to ="auth/autonomy">Автономия</Link>
+          </div>
+          
+     <div className={style.events}>
+            <Link to="auth/events">Мероприятия</Link>
+          </div>
+
+     <div className={style.news_editor}>
+            <Link to="auth/">Редакор Новостей</Link>
+          </div>
+    
+
+    <div className={style.event_editor}>
+            <Link to="auth/">Редакор Мероприятий </Link>
+          </div>
+       
+          <button className={style.button} 
+        onClick={handleButtonClick}>
+        На главную станицу
+      </button>
+       
+         </div>
+     <Footer/> 
+   </div>
+   
+
+  );
+};
+
+export default AdminPages;
+
