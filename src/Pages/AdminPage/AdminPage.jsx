46ef80237e281766d45c060ada3ef590b20f4076
Aizhan  Mysakanova <117911188+Aizhan-px@users.noreply.github.com>
2024-10-18 10:24:22 +0200

    Revert "created the administrative page and worked on the SCSS"

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -1,79 +1,19 @@
 import style from "./AdminPage.module.scss";
-import { Routes, useNavigate } from "react-router-dom";
-import App from "../../App/App";
-import Charity from "../Charity/Charity";
-import News from "../News/News";
-import { Link } from "react-router-dom";
-import Footer from "../../Components/Footer/Footer";
-import Header from "../../Components/Header/Header";
-import logo from "../../assets/images/logo.webp";
-import adminPageIcon from '../../assets/images/adminPageIcon.svg';
-// src/assets/images/adminPageIcon.svg';
+import {useNavigate} from "react-router-dom";
 
-const AdminPages = () => {
-  
-
-  const navigate = useNavigate();
+const AdminPage = () => {
+const navigate = useNavigate(); 
   const handleButtonClick = () => {
-    navigate("/");
+    navigate("/"); 
   };
-  
-  return (
-    
-    <div className={style.container}>
-      <div className={style.headerAdmin}>
-      <div className={style.logo__container}>
-          <img src={logo} alt="Logo" className={style.logo} />
+    return (
+        <div className={style.container}>
+             <button className={style.button} onClick={handleButtonClick}>
+              На главную станицу
+            </button>
+            <p className={style.title}> Здесь будет страница администратора</p>
         </div>
+    )
+}
 
-        <div >
-          <p className={style.p}>Админ станица</p>
-
-        </div>
-      <div>
-         <button className={style.button} 
-        onClick={handleButtonClick}>
-        На главную станицу
-      </button>
-      </div>
-      </div>
-
-   <div className={style.adminBlock_container}>
-    
-      <div className={style.charity}>
-            <Link to="auth/charity"  className={style.castomLink}>Благотворительность</Link>
-          </div>
-
-     <div className={style.autonomy}>
-            <Link to ="auth/autonomy" className={style.castomLink}>Автономия</Link>
-          </div>
-          
-     <div className={style.events}>
-            <Link to="auth/events" className={style.castomLink}>Мероприятия</Link>
-          </div>
-
-     <div className={style.news_editor }>
-            <Link to="auth/" className={style.castomLink}>Редакор Новостей</Link>
-          </div>
-    
-
-    <div className={style.event_editor}>
-            <Link to="auth/" className={style.castomLink}>Редакор Мероприятий </Link>
-   </div>
-
-   {/* <div>
-   <img src={adminPageIcon.svg} alt="adminPageIcon" className={style.adminPageIcon}/>
-   </div>
-        */}
-         
-       
-         </div>
-     <Footer/> 
-   </div>
-   
-
-  );
-};
-
-export default AdminPages;
-
+export default AdminPage;
